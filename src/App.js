30e58feac5ffd4b9c8dc40f8cc2a9efbdc79e181
TommySosa <tomas.facundo.sosa@gmail.com>
2023-06-25 21:37:36 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Prueba from './components/Prueba';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Muestra el compontente Prueba y le asigna los valores por probs para a que sea 10 y b 20 */}
+      <Prueba a={50} b={20}></Prueba>
     </div>
   );
 }
---
+++ src/components/Prueba.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+
+function Prueba(props){
+    // Recibe los valores desde las props y los guarda en una constante a y otra en b
+    const {a , b} = props
+    // Suma los valores y los guarda en una variable
+    let resultado = a + b
+
+    const [result, setResult] = useState()
+    const [contador, setContador] = useState(0)
+
+    const resta = (a,b) => {
+        return a - b
+    }
+
+    const handleResta = () => {
+        setResult(resta(a,b))
+    }
+
+    const handleContador = () => {
+        setContador(contador + 1)
+    }
+
+    //cuando inicia la aplicacion
+   
+    useEffect(() => {
+        document.title = 'Clicks : ' + contador
+    }, [contador])
+    useEffect(() => {
+        document.title = 'Titulo al iniciar'
+    }, [])  
+    return(
+        <div>
+            <h1>Prueba</h1>
+            {/* Muestra el resultado en lenguaje JSX entre {} el valor de la variable resultado */}
+            <h2>{resultado}</h2>
+            <button onClick={handleResta}>Restar</button>
+            <h3>{result}</h3>
+            <button onClick={handleContador}>Click aqui</button>
+            <h4>{contador}</h4>
+            {/* Operador ternario 
+            condicion ? muestra si es verdadero : muestra si es falso */}
+            {contador === 5 ? <h1>Contador en 5</h1> : <h1>El contador no es 5</h1>} 
+        </div>
+    )
+}
+
+export default Prueba
